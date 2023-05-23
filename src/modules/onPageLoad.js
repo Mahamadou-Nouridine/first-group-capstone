d7e22f48ceaa53414a3d0f170a5ee01b9419de1b
mosidrum <mosiokanga@gmail.com>
2023-05-23 15:50:26 +0100

    Fix linter errors

--- src/modules/onPageLoad.js
+++ src/modules/onPageLoad.js
@@ -5,7 +5,7 @@ const onPageLoad = async () => {
     },
   });
   const data = await res.json();
-  const urls = Object.values(data).map(catImage => (catImage.url));
+  const urls = Object.values(data).map((catImage) => (catImage.url));
   let show = '';
   const holder = document.querySelector('.cat-container');
   urls.forEach((url) => {
@@ -18,16 +18,16 @@ const onPageLoad = async () => {
   holder.innerHTML = show;
 };
 
-const itemCounter = async() => {
+const itemCounter = async () => {
   const res = await fetch('https://api.thecatapi.com/v1/images/search?breed_ids=beng&limit=20&order=ASC', {
     headers: {
       'x-api-key': 'live_3LSh5c98OlFcR32CElsjBzHpY2E51GK0xqpsu3c0dmxVtEwQ7m9HCw6g5i7VPjBr',
     },
-  })
+  });
   const data = await res.json();
-  const urls = Object.values(data).map(catImage => catImage.url);
+  const urls = Object.values(data).map((catImage) => (catImage.url));
   const itemCount = document.querySelector('.item-count');
   itemCount.innerHTML = urls.length;
-}
+};
 
-export { onPageLoad, itemCounter};
+export { onPageLoad, itemCounter };
