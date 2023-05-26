3aa28ff7cad3f805f9f3e76e4bd0312e0694ad5d
mosidrum <mosiokanga@gmail.com>
2023-05-26 13:30:38 +0100

    Resolving conflicts

--- src/modules/constant.js
+++
@@ -1,7 +0,0 @@
-const appId = '0okofbMEcTk3lkNblPoL';
-
-export const baseUrl = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}`;
-export const endPoints = {
-  comments: '/comments',
-  likes: '/likes',
-};
